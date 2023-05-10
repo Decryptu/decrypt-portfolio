edc35e4252e7205d7a05dfce8a0e949e56feabc6
Decrypt <louis.vicomte@protonmail.com>
2023-05-10 23:01:00 +0200

    fix socials

--- app/projects/[slug]/header.tsx
+++ app/projects/[slug]/header.tsx
@@ -67,7 +67,7 @@ export const Header: React.FC<Props> = ({ project, views }) => {
 								views,
 							)}
 						</span>
-						<Link target="_blank" href="https://twitter/decrypttv">
+						<Link target="_blank" href="https://twitter.com/decrypttv">
 							<Twitter
 								className={`w-6 h-6 duration-200 hover:font-medium ${
 									isIntersecting
@@ -76,7 +76,7 @@ export const Header: React.FC<Props> = ({ project, views }) => {
 								} `}
 							/>
 						</Link>
-						<Link target="_blank" href="https://dribbble.com/decryptu">
+						<Link target="_blank" href="https://dribbble.com/decrypt">
 							<Dribbble
 								className={`w-6 h-6 duration-200 hover:font-medium ${
 									isIntersecting
