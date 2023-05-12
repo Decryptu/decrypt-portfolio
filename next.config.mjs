ba3d07d4137631c835d82ee9070b94402b6b3678
Decrypt <louis.vicomte@protonmail.com>
2023-05-12 15:56:30 +0200

    fix pls

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,4 @@
 import { withContentlayer } from "next-contentlayer";
-import withPWA from 'next-pwa';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -8,12 +7,6 @@ const nextConfig = {
 		appDir: true,
 		mdxRs: true,
 	},
-	pwa: {
-		dest: 'public', // directory where the service worker will be served
-		disable: process.env.NODE_ENV === 'development',
-		register: true,
-		skipWaiting: true,
-	}
 };
 
-export default withContentlayer(withPWA(nextConfig));
+export default withContentlayer(nextConfig);
