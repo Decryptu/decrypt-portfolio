63a2df00c5ea11f9d8927d2b2ccf5f99782f3eca
Decrypt <louis.vicomte@protonmail.com>
2023-05-12 16:22:09 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,15 @@
-import { withContentlayer } from "next-contentlayer";
+import { withContentlayer } from 'next-contentlayer'
+import withPWA from 'next-pwa'
 
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-	pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
-	experimental: {
-		appDir: true,
-		mdxRs: true,
-	},
-};
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
+  experimental: {
+    appDir: true,
+    mdxRs: true,
+  },
+  pwa: {
+    dest: 'public', // this can be set to 'public' or 'static' depending on your preference
+  },
+}
 
-export default withContentlayer(nextConfig);
+export default withPWA(withContentlayer(nextConfig))
