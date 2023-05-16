21c3608f01cf7473a9e18ac28b5b9fdb8edfa6c6
Decrypt <louis.vicomte@protonmail.com>
2023-05-16 20:46:09 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,4 @@
 import { withContentlayer } from "next-contentlayer";
-import sitemap from 'nextjs-sitemap-generator';
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
 import { promises as fs } from 'fs';
@@ -7,23 +6,26 @@ import { promises as fs } from 'fs';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const generateDynamicPages = async () => {
+const generateSitemap = async () => {
+  const pages = ['/', '/contact', '/projects'];
+
   const directoryPath = join(__dirname, 'content', 'projects');
   const files = await fs.readdir(directoryPath);
   const slugs = files.map(file => file.replace('.mdx', ''));
-  return slugs.map(slug => `/projects/${slug}`);
-};
-
-const generateSitemap = async () => {
-  const dynamicPages = await generateDynamicPages();
-  const pages = ['/', ...dynamicPages];
-
-  sitemap({
-    baseUrl: 'https://decrypt.im',
-    pagesDirectory: join(__dirname, 'app'),
-    pages,
-    targetDirectory: 'public',
+  const projectPages = slugs.map(slug => `/projects/${slug}`);
+  
+  const allPages = [...pages, ...projectPages];
+  const baseUrl = 'https://decrypt.im';
+  
+  let sitemap = `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n`;
+  
+  allPages.forEach(page => {
+    sitemap += `<url>\n<loc>${baseUrl}${page}</loc>\n<lastmod>${new Date().toISOString()}</lastmod>\n</url>\n`;
   });
+
+  sitemap += `</urlset>`;
+  
+  await fs.writeFile(join(__dirname, 'public', 'sitemap.xml'), sitemap);
 };
 
 generateSitemap();
