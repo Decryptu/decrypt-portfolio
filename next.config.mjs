10f3a5debad2f3b9a331f3384be3c8bb76a2126e
Decrypt <louis.vicomte@protonmail.com>
2023-05-16 20:04:07 +0200

    add sitemap

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,19 @@
 import { withContentlayer } from "next-contentlayer";
+import sitemap from 'nextjs-sitemap-generator';
+
+sitemap({  
+  baseUrl: 'https://decrypt.im/',  
+  pagesDirectory: __dirname + "/pages",  
+  targetDirectory : 'public/'  
+});
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
-	experimental: {
-		appDir: true,
-		mdxRs: true,
-	},
+  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+  experimental: {
+    appDir: true,
+    mdxRs: true,
+  },
 };
 
 export default withContentlayer(nextConfig);
