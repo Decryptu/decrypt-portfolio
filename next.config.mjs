e2015e1473b485faf91d473858ab8aea97dbd7f9
Decrypt <louis.vicomte@protonmail.com>
2023-05-16 20:25:36 +0200

    update sitemap for dynamic

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,31 @@
 import { withContentlayer } from "next-contentlayer";
 import sitemap from 'nextjs-sitemap-generator';
 import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+import { dirname, join } from 'path';
+import { promises as fs } from 'fs';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-sitemap({  
-  baseUrl: 'https://decrypt.im',  
-  pagesDirectory: __dirname + "/pages",  
-  targetDirectory : 'public/'  
-});
+const generateDynamicPages = async () => {
+  const directoryPath = join(__dirname, 'content', 'projects');
+  const files = await fs.readdir(directoryPath);
+  const slugs = files.map(file => file.replace('.mdx', ''));
+  return slugs.map(slug => `/projects/${slug}`);
+};
+
+const generateSitemap = async () => {
+  const dynamicPages = await generateDynamicPages();
+  const pages = ['/', ...dynamicPages];
+
+  sitemap({
+    baseUrl: 'https://decrypt.im',
+    pages,
+    targetDirectory: 'public',
+  });
+};
+
+generateSitemap();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
