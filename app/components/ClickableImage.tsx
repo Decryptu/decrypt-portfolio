4436a5c3dfe1412c3fb3fc8f3c2c020bf863e812
Decrypt <louis.vicomte@protonmail.com>
2023-06-27 23:03:52 +0200

    mobile better img

--- app/components/ClickableImage.tsx
+++ app/components/ClickableImage.tsx
@@ -32,10 +32,10 @@ const ClickableImage = ({ src, alt }: ClickableImageProps) => {
       />
       {show && (
         <div
-          className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex items-center justify-center z-50"
+          className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex items-center justify-center z-50 overflow-x-auto"
           onClick={handleClose}
         >
-          <img src={src} alt={alt} className="max-w-full h-[95vh] object-contain" />
+          <img src={src} alt={alt} className="h-[95vh] md:h-[95vh] object-contain" />
         </div>
       )}
     </>
