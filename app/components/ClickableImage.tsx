383509e169900f4ad398c821ce116bdafb104108
Decrypt <louis.vicomte@protonmail.com>
2024-03-09 12:26:36 +0100

    close lightbox with esc

--- app/components/ClickableImage.tsx
+++ app/components/ClickableImage.tsx
@@ -1,49 +1,50 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 
 interface ClickableImageProps {
-	src: string;
-	alt?: string;
+  src: string;
+  alt?: string;
 }
 
-const ClickableImage = ({ src, alt }: ClickableImageProps) => {
-	const [show, setShow] = useState(false);
+const ClickableImage: React.FC<ClickableImageProps> = ({ src, alt }) => {
+  const [show, setShow] = useState(false);
 
-	useEffect(() => {
-		document.body.style.overflow = show ? "hidden" : "auto";
-	}, [show]);
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.key === 'Escape') {
+        setShow(false);
+      }
+    };
 
-	const handleClick = () => {
-		setShow(true);
-	};
+    // Effect to handle the Escape key
+    document.body.style.overflow = show ? 'hidden' : 'auto';
+    window.addEventListener('keydown', handleKeyDown);
+    return () => window.removeEventListener('keydown', handleKeyDown);
+  }, [show]);
 
-	const handleClose = () => {
-		setShow(false);
-	};
-
-	return (
-		<>
-			<img
-				src={src}
-				alt={alt}
-				onClick={handleClick}
-				className="cursor-pointer rounded-md border border-zinc-200"
-			/>
-			{show && (
-				<div
-					className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex items-center justify-center z-50"
-					onClick={handleClose}
-				>
-					<img
-						src={src}
-						alt={alt}
-						className="max-w-full h-[95vh] object-contain"
-					/>
-				</div>
-			)}
-		</>
-	);
+  return (
+    <>
+      <img
+        src={src}
+        alt={alt || 'image'}
+        onClick={() => setShow(true)}
+        className="cursor-pointer rounded-md border border-zinc-200"
+      />
+      {show && (
+        <div
+          className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex items-center justify-center z-50"
+          onClick={() => setShow(false)}
+        >
+          <img
+            src={src}
+            alt={alt || 'image'}
+            className="max-w-full h-[95vh] object-contain"
+          />
+        </div>
+      )}
+    </>
+  );
 };
 
 export default ClickableImage;
