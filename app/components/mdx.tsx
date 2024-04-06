5949c3cb317cde4c3dacfc3e7b41c62d4bc66ffb
Decrypt <louis.vicomte@protonmail.com>
2024-04-07 01:46:34 +0200

    fix clickableimage src

--- app/components/mdx.tsx
+++ app/components/mdx.tsx
@@ -6,176 +6,179 @@ import type * as React from "react";
 import ClickableImage from "./ClickableImage";
 
 interface ComponentProps extends React.HTMLAttributes<HTMLElement> {
-    className?: string;
+  className?: string;
 }
 
 // Define a new interface for Link props that includes the href property
 interface LinkProps extends ComponentProps {
-    href: string; // Making href a required property
+  href: string; // Making href a required property
 }
 
 function clsx(...args: any) {
-	return args.filter(Boolean).join(" ");
+  return args.filter(Boolean).join(" ");
 }
 const components = {
-    h1: ({ className, ...props }: ComponentProps) => (
-		<h1
-			className={clsx(
-				"mt-2 scroll-m-20 text-4xl font-bold tracking-tight",
-				className,
-			)}
-			{...props}
-		/>
-	),
-	h2: ({ className, ...props }: ComponentProps) => (
-		<h2
-			className={clsx(
-				"mt-10 scroll-m-20 border-b border-b-zinc-800 pb-1 text-3xl font-semibold tracking-tight first:mt-0",
-				className,
-			)}
-			{...props}
-		/>
-	),
-	h3: ({ className, ...props }: ComponentProps) => (
-		<h3
-			className={clsx(
-				"mt-8 scroll-m-20 text-2xl font-semibold tracking-tight",
-				className,
-			)}
-			{...props}
-		/>
-	),
-	h4: ({ className, ...props }: ComponentProps) => (
-		<h4
-			className={clsx(
-				"mt-8 scroll-m-20 text-xl font-semibold tracking-tight",
-				className,
-			)}
-			{...props}
-		/>
-	),
-	h5: ({ className, ...props }: ComponentProps) => (
-		<h5
-			className={clsx(
-				"mt-8 scroll-m-20 text-lg font-semibold tracking-tight",
-				className,
-			)}
-			{...props}
-		/>
-	),
-	h6: ({ className, ...props }: ComponentProps) => (
-		<h6
-			className={clsx(
-				"mt-8 scroll-m-20 text-base font-semibold tracking-tight",
-				className,
-			)}
-			{...props}
-		/>
-	),
-    a: ({ className, href, ...props }: LinkProps) => (
-        <Link href={href} passHref>
-            <a
-                className={clsx(
-                    "font-medium text-zinc-900 underline underline-offset-4",
-                    className,
-                )}
-                {...props}
-            />
-        </Link>
-    ),
-	p: ({ className, ...props }: ComponentProps) => (
-		<p
-			className={clsx("leading-7 [&:not(:first-child)]:mt-6", className)}
-			{...props}
-		/>
-	),
-	ul: ({ className, ...props }: ComponentProps) => (
-		<ul className={clsx("my-6 ml-6 list-disc", className)} {...props} />
-	),
-	ol: ({ className, ...props }: ComponentProps) => (
-		<ol className={clsx("my-6 ml-6 list-decimal", className)} {...props} />
-	),
-	li: ({ className, ...props }: ComponentProps) => (
-		<li className={clsx("mt-2", className)} {...props} />
-	),
-	blockquote: ({ className, ...props }: ComponentProps) => (
-		<blockquote
-			className={clsx(
-				"mt-6 border-l-2 border-zinc-300 pl-6 italic text-zinc-800 [&>*]:text-zinc-600",
-				className,
-			)}
-			{...props}
-		/>
-	),
-	img: (props: React.ImgHTMLAttributes<HTMLImageElement>) => (
-		<ClickableImage {...props} />
-	),
-	hr: ({ ...props }) => (
-		<hr className="my-4 border-zinc-200 md:my-8" {...props} />
-	),
-	table: ({ className, ...props }: React.HTMLAttributes<HTMLTableElement>) => (
-		<div className="w-full my-6 overflow-y-auto">
-			<table className={clsx("w-full", className)} {...props} />
-		</div>
-	),
-	tr: ({ className, ...props }: React.HTMLAttributes<HTMLTableRowElement>) => (
-		<tr
-			className={clsx(
-				"m-0 border-t border-zinc-300 p-0 even:bg-zinc-100",
-				className,
-			)}
-			{...props}
-		/>
-	),
-	th: ({ className, ...props }: ComponentProps) => (
-		<th
-			className={clsx(
-				"border border-zinc-200 px-4 py-2 text-left font-bold [&[align=center]]:text-center [&[align=right]]:text-right",
-				className,
-			)}
-			{...props}
-		/>
-	),
-	td: ({ className, ...props }: ComponentProps) => (
-		<td
-			className={clsx(
-				"border border-zinc-200 px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right",
-				className,
-			)}
-			{...props}
-		/>
-	),
-	pre: ({ className, ...props }: ComponentProps) => (
-		<pre
-			className={clsx(
-				"mt-6 mb-4 overflow-x-auto rounded-lg bg-zinc-900 py-4",
-				className,
-			)}
-			{...props}
-		/>
-	),
-	code: ({ className, ...props }: ComponentProps) => (
-		<code
-			className={clsx(
-				"relative rounded border bg-zinc-300 bg-opacity-25 py-[0.2rem] px-[0.3rem] font-mono text-sm text-zinc-600",
-				className,
-			)}
-			{...props}
-		/>
-	),
-	Image,
+  h1: ({ className, ...props }: ComponentProps) => (
+    <h1
+      className={clsx(
+        "mt-2 scroll-m-20 text-4xl font-bold tracking-tight",
+        className
+      )}
+      {...props}
+    />
+  ),
+  h2: ({ className, ...props }: ComponentProps) => (
+    <h2
+      className={clsx(
+        "mt-10 scroll-m-20 border-b border-b-zinc-800 pb-1 text-3xl font-semibold tracking-tight first:mt-0",
+        className
+      )}
+      {...props}
+    />
+  ),
+  h3: ({ className, ...props }: ComponentProps) => (
+    <h3
+      className={clsx(
+        "mt-8 scroll-m-20 text-2xl font-semibold tracking-tight",
+        className
+      )}
+      {...props}
+    />
+  ),
+  h4: ({ className, ...props }: ComponentProps) => (
+    <h4
+      className={clsx(
+        "mt-8 scroll-m-20 text-xl font-semibold tracking-tight",
+        className
+      )}
+      {...props}
+    />
+  ),
+  h5: ({ className, ...props }: ComponentProps) => (
+    <h5
+      className={clsx(
+        "mt-8 scroll-m-20 text-lg font-semibold tracking-tight",
+        className
+      )}
+      {...props}
+    />
+  ),
+  h6: ({ className, ...props }: ComponentProps) => (
+    <h6
+      className={clsx(
+        "mt-8 scroll-m-20 text-base font-semibold tracking-tight",
+        className
+      )}
+      {...props}
+    />
+  ),
+  a: ({ className, href, ...props }: LinkProps) => (
+    <Link href={href} passHref>
+      <a
+        className={clsx(
+          "font-medium text-zinc-900 underline underline-offset-4",
+          className
+        )}
+        {...props}
+      />
+    </Link>
+  ),
+  p: ({ className, ...props }: ComponentProps) => (
+    <p
+      className={clsx("leading-7 [&:not(:first-child)]:mt-6", className)}
+      {...props}
+    />
+  ),
+  ul: ({ className, ...props }: ComponentProps) => (
+    <ul className={clsx("my-6 ml-6 list-disc", className)} {...props} />
+  ),
+  ol: ({ className, ...props }: ComponentProps) => (
+    <ol className={clsx("my-6 ml-6 list-decimal", className)} {...props} />
+  ),
+  li: ({ className, ...props }: ComponentProps) => (
+    <li className={clsx("mt-2", className)} {...props} />
+  ),
+  blockquote: ({ className, ...props }: ComponentProps) => (
+    <blockquote
+      className={clsx(
+        "mt-6 border-l-2 border-zinc-300 pl-6 italic text-zinc-800 [&>*]:text-zinc-600",
+        className
+      )}
+      {...props}
+    />
+  ),
+  img: ({
+    src,
+    ...props
+  }: React.ImgHTMLAttributes<HTMLImageElement> & { src: string }) => (
+    <ClickableImage src={src || ""} alt={props.alt} />
+  ),
+  hr: ({ ...props }) => (
+    <hr className="my-4 border-zinc-200 md:my-8" {...props} />
+  ),
+  table: ({ className, ...props }: React.HTMLAttributes<HTMLTableElement>) => (
+    <div className="w-full my-6 overflow-y-auto">
+      <table className={clsx("w-full", className)} {...props} />
+    </div>
+  ),
+  tr: ({ className, ...props }: React.HTMLAttributes<HTMLTableRowElement>) => (
+    <tr
+      className={clsx(
+        "m-0 border-t border-zinc-300 p-0 even:bg-zinc-100",
+        className
+      )}
+      {...props}
+    />
+  ),
+  th: ({ className, ...props }: ComponentProps) => (
+    <th
+      className={clsx(
+        "border border-zinc-200 px-4 py-2 text-left font-bold [&[align=center]]:text-center [&[align=right]]:text-right",
+        className
+      )}
+      {...props}
+    />
+  ),
+  td: ({ className, ...props }: ComponentProps) => (
+    <td
+      className={clsx(
+        "border border-zinc-200 px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right",
+        className
+      )}
+      {...props}
+    />
+  ),
+  pre: ({ className, ...props }: ComponentProps) => (
+    <pre
+      className={clsx(
+        "mt-6 mb-4 overflow-x-auto rounded-lg bg-zinc-900 py-4",
+        className
+      )}
+      {...props}
+    />
+  ),
+  code: ({ className, ...props }: ComponentProps) => (
+    <code
+      className={clsx(
+        "relative rounded border bg-zinc-300 bg-opacity-25 py-[0.2rem] px-[0.3rem] font-mono text-sm text-zinc-600",
+        className
+      )}
+      {...props}
+    />
+  ),
+  Image,
 };
 
 interface MdxProps {
-	code: string;
+  code: string;
 }
 
 export function Mdx({ code }: MdxProps) {
-	const Component = useMDXComponent(code);
+  const Component = useMDXComponent(code);
 
-	return (
-		<div className="mdx">
-			<Component components={components} />
-		</div>
-	);
+  return (
+    <div className="mdx">
+      <Component components={components as any} />
+    </div>
+  );
 }
