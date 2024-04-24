658f138427f97d35ff35ac4df4e8fbee10071452
Decrypt <louis.vicomte@protonmail.com>
2024-04-24 18:22:52 +0200

    Update ClickableImage.tsx

--- app/components/ClickableImage.tsx
+++ app/components/ClickableImage.tsx
@@ -8,61 +8,59 @@ interface ClickableImageProps {
   alt?: string;
 }
 
-const ClickableImage: React.FC<ClickableImageProps> = ({ src, alt }) => {
+const ClickableImage: React.FC<ClickableImageProps> = ({
+  src,
+  alt = "image",
+}) => {
   const [show, setShow] = useState(false);
 
   useEffect(() => {
-    if (show) {
-      const handleKeyDown = (event: KeyboardEvent) => {
-        if (event.key === "Escape") {
-          setShow(false);
-        }
-      };
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.key === "Escape") {
+        setShow(false);
+      }
+    };
 
+    if (show) {
       document.body.style.overflow = "hidden";
       window.addEventListener("keydown", handleKeyDown);
-      return () => {
-        window.removeEventListener("keydown", handleKeyDown);
-        document.body.style.overflow = "auto";
-      };
     }
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+      document.body.style.overflow = "auto";
+    };
   }, [show]);
 
+  const lightboxElement = (
+    <div
+      className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50"
+      onClick={() => setShow(false)}
+      tabIndex={-1}
+      role="dialog"
+      aria-modal="true"
+      aria-labelledby="lightboxImage"
+    >
+      <img
+        id="lightboxImage"
+        src={src}
+        alt={alt}
+        className="max-w-full h-[95vh] object-contain"
+      />
+    </div>
+  );
+
   const lightbox =
-    show &&
-    ReactDOM.createPortal(
-      <div
-        className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50"
-        onClick={() => setShow(false)}
-        onKeyDown={(event) => {
-          if (event.key === "Escape") {
-            setShow(false);
-          }
-        }}
-        tabIndex={-1} // Makes it focusable
-        role="dialog"
-        aria-modal="true"
-        aria-labelledby="lightboxImage"
-      >
-        <img
-          id="lightboxImage"
-          src={src}
-          alt={alt || "image"}
-          className="max-w-full h-[95vh] object-contain"
-        />
-      </div>,
-      document.body
-    );
+    show && ReactDOM.createPortal(lightboxElement, document.body);
 
   return (
     <>
       <img
         src={src}
-        alt={alt || "image"}
+        alt={alt}
         onClick={() => setShow(true)}
         className="cursor-pointer rounded-md border border-zinc-200 dark:border-zinc-800"
         role="button"
-        tabIndex={0} // Makes it focusable
+        tabIndex={0}
       />
       {lightbox}
     </>
