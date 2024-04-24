739f23c7d313bb40ace4e77d0073fe0d241e44b5
Decrypt <louis.vicomte@protonmail.com>
2024-04-24 18:16:49 +0200

    Enhance lightbox
    focusable, and code opti

--- app/components/ClickableImage.tsx
+++ app/components/ClickableImage.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import type React from "react";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
+
 interface ClickableImageProps {
   src: string;
   alt?: string;
@@ -12,33 +12,47 @@ const ClickableImage: React.FC<ClickableImageProps> = ({ src, alt }) => {
   const [show, setShow] = useState(false);
 
   useEffect(() => {
-    const handleKeyDown = (event: KeyboardEvent) => {
-      if (event.key === "Escape") {
-        setShow(false);
-      }
-    };
+    if (show) {
+      const handleKeyDown = (event: KeyboardEvent) => {
+        if (event.key === "Escape") {
+          setShow(false);
+        }
+      };
 
-    document.body.style.overflow = show ? "hidden" : "auto";
-    window.addEventListener("keydown", handleKeyDown);
-    return () => window.removeEventListener("keydown", handleKeyDown);
+      document.body.style.overflow = "hidden";
+      window.addEventListener("keydown", handleKeyDown);
+      return () => {
+        window.removeEventListener("keydown", handleKeyDown);
+        document.body.style.overflow = "auto";
+      };
+    }
   }, [show]);
 
-  // Here we use React Portal to render the lightbox outside the normal DOM hierarchy
-  const lightbox = show
-    ? ReactDOM.createPortal(
-        <div
-          className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex items-center justify-center z-50"
-          onClick={() => setShow(false)}
-        >
-          <img
-            src={src}
-            alt={alt || "image"}
-            className="max-w-full h-[95vh] object-contain"
-          />
-        </div>,
-        document.body // Target container is the body element
-      )
-    : null;
+  const lightbox =
+    show &&
+    ReactDOM.createPortal(
+      <div
+        className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50"
+        onClick={() => setShow(false)}
+        onKeyDown={(event) => {
+          if (event.key === "Escape") {
+            setShow(false);
+          }
+        }}
+        tabIndex={-1} // Makes it focusable
+        role="dialog"
+        aria-modal="true"
+        aria-labelledby="lightboxImage"
+      >
+        <img
+          id="lightboxImage"
+          src={src}
+          alt={alt || "image"}
+          className="max-w-full h-[95vh] object-contain"
+        />
+      </div>,
+      document.body
+    );
 
   return (
     <>
@@ -47,6 +61,8 @@ const ClickableImage: React.FC<ClickableImageProps> = ({ src, alt }) => {
         alt={alt || "image"}
         onClick={() => setShow(true)}
         className="cursor-pointer rounded-md border border-zinc-200 dark:border-zinc-800"
+        role="button"
+        tabIndex={0} // Makes it focusable
       />
       {lightbox}
     </>
