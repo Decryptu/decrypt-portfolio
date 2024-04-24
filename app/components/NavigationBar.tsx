0da63053dd3ad3d1f99c59b3c48012689fa18c2f
Decrypt <louis.vicomte@protonmail.com>
2024-04-24 19:47:46 +0200

    enhance nav menu + dark mode

--- app/components/NavigationBar.tsx
+++ app/components/NavigationBar.tsx
@@ -11,34 +11,35 @@ import {
 } from "lucide-react";
 
 const NavigationBar = () => {
-  const [currentPath, setCurrentPath] = useState<string>("/");
-  const [isSubMenuVisible, setSubMenuVisible] = useState<boolean>(false);
-  const [isFirstLoad, setFirstLoad] = useState<boolean>(true);
-  const [tooltip, setTooltip] = useState<string | null>(null);
+  const [state, setState] = useState<{
+    currentPath: string;
+    isSubMenuVisible: boolean;
+    isFirstLoad: boolean;
+    tooltip: string | null;
+  }>({
+    currentPath: "/",
+    isSubMenuVisible: false,
+    isFirstLoad: true,
+    tooltip: null,
+  });
   const ellipsisRef = useRef<HTMLButtonElement>(null);
   const subMenuRef = useRef<HTMLDivElement>(null);
 
-  // Utility to determine if the device is mobile
-  const isMobile = () => window.innerWidth < 768 || "ontouchstart" in window;
-
   useEffect(() => {
-    // Set the initial path based on the window's location to sync server/client rendering
     if (typeof window !== "undefined") {
-      const path = window.location.pathname;
-      setCurrentPath(path);
-      setFirstLoad(false);
+      setState((prev) => ({
+        ...prev,
+        currentPath: window.location.pathname,
+        isFirstLoad: false,
+      }));
     }
   }, []);
 
-  const handleLinkClick = (path: string) => {
-    setCurrentPath(path); // Update current path on click
-  };
+  const handleLinkClick = (path: string) =>
+    setState((prev) => ({ ...prev, currentPath: path }));
+  const handleEllipsisClick = () =>
+    setState((prev) => ({ ...prev, isSubMenuVisible: !prev.isSubMenuVisible }));
 
-  const handleEllipsisClick = () => {
-    setSubMenuVisible(!isSubMenuVisible);
-  };
-
-  // This effect sets up an event listener for clicks on the document
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
       if (
@@ -47,100 +48,81 @@ const NavigationBar = () => {
         !subMenuRef.current.contains(event.target as Node) &&
         !ellipsisRef.current.contains(event.target as Node)
       ) {
-        setSubMenuVisible(false);
+        setState((prev) => ({ ...prev, isSubMenuVisible: false }));
       }
     };
 
-    // Add event listener when the submenu is visible
-    if (isSubMenuVisible) {
+    if (state.isSubMenuVisible) {
       document.addEventListener("mousedown", handleClickOutside);
     }
 
-    // Cleanup event listener
     return () => {
       document.removeEventListener("mousedown", handleClickOutside);
     };
-  }, [isSubMenuVisible]); // Re-run effect if isSubMenuVisible changes
+  }, [state.isSubMenuVisible]);
+
+  const navItems = [
+    { path: "/", Icon: Home, label: "Home" },
+    { path: "/projects", Icon: BriefcaseBusiness, label: "Projects" },
+    { path: "/contact", Icon: Contact, label: "Contact" },
+  ];
+
+  const isMobile = () => window.innerWidth < 768 || "ontouchstart" in window;
 
   return (
     <div
       className={`fixed inset-x-0 bottom-4 flex justify-center items-center navbar-parent transition-opacity duration-1000 ${
-        isFirstLoad ? "opacity-0" : "opacity-100"
+        state.isFirstLoad ? "opacity-0" : "opacity-100"
       } animate-fadeIn`}
       style={{ zIndex: 1000 }}
     >
-      <div className="flex items-center space-x-2 rounded-lg bg-white p-1 shadow-[0_3px_10px_rgb(0,0,0,0.1)]">
-        {/* Home Icon */}
-        <Link href="/" passHref legacyBehavior>
-          <a
-            className={`relative flex items-center justify-center w-8 h-8 ${
-              currentPath === "/" ? "bg-zinc-200 rounded-md" : ""
-            }`}
-            onMouseEnter={() => !isMobile() && setTooltip("Home")}
-            onMouseLeave={() => setTooltip(null)}
-            onClick={() => handleLinkClick("/")}
-          >
-            <Home strokeWidth={1.25} className="w-6 h-6" />
-            {tooltip === "Home" && (
-              <span className="absolute -top-10 left-1/2 transform -translate-x-1/2 px-2 py-1 bg-white text-black text-xs rounded shadow-lg">
-                Home
-              </span>
-            )}
-          </a>
-        </Link>
-
-        {/* Briefcase Icon */}
-        <Link href="/projects" passHref legacyBehavior>
-          <a
-            className={`relative flex items-center justify-center w-8 h-8 ${
-              currentPath === "/projects" ? "bg-zinc-200 rounded-md" : ""
-            }`}
-            onMouseEnter={() => !isMobile() && setTooltip("Projects")}
-            onMouseLeave={() => setTooltip(null)}
-            onClick={() => handleLinkClick("/projects")}
-          >
-            <BriefcaseBusiness strokeWidth={1.25} className="w-6 h-6" />
-            {tooltip === "Projects" && (
-              <span className="absolute -top-10 left-1/2 transform -translate-x-1/2 px-2 py-1 bg-white text-black text-xs rounded shadow-lg">
-                Projects
-              </span>
-            )}
-          </a>
-        </Link>
-
-        {/* Contact Icon */}
-        <Link href="/contact" passHref legacyBehavior>
-          <a
-            className={`relative flex items-center justify-center w-8 h-8 ${
-              currentPath === "/contact" ? "bg-zinc-200 rounded-md" : ""
-            }`}
-            onMouseEnter={() => !isMobile() && setTooltip("Contact")}
-            onMouseLeave={() => setTooltip(null)}
-            onClick={() => handleLinkClick("/contact")}
-          >
-            <Contact strokeWidth={1.25} className="w-6 h-6" />
-            {tooltip === "Contact" && (
-              <span className="absolute -top-10 left-1/2 transform -translate-x-1/2 px-2 py-1 bg-white text-black text-xs rounded shadow-lg">
-                Contact
-              </span>
-            )}
-          </a>
-        </Link>
-
+      <div className="flex items-center space-x-2 rounded-lg border dark:border-zinc-800 border-zinc-300 dark:bg-zinc-900 bg-white p-1 shadow-[0_3px_10px_rgb(0,0,0,0.1)] dark:shadow-[0_3px_10px_rgb(0,0,0,0.5)]">
+        {navItems.map(({ path, Icon, label }) => (
+          <Link key={path} href={path} passHref legacyBehavior>
+            <a
+              className={`relative flex items-center justify-center w-8 h-8 dark:hover:bg-zinc-800 hover:bg-zinc-200 rounded-md ${
+                state.currentPath === path
+                  ? "bg-zinc-200 dark:bg-zinc-950 rounded-md"
+                  : ""
+              }`}
+              onMouseEnter={() =>
+                !isMobile() && setState((prev) => ({ ...prev, tooltip: label }))
+              }
+              onMouseLeave={() =>
+                setState((prev) => ({ ...prev, tooltip: null }))
+              }
+              onClick={() => handleLinkClick(path)}
+            >
+              <Icon
+                strokeWidth={1.25}
+                className="w-6 h-6 dark:stroke-zinc-100 stroke-zinc-950"
+              />
+              {state.tooltip === label && (
+                <span className="absolute -top-10 left-1/2 transform -translate-x-1/2 px-2 py-1 dark:bg-zinc-900 dark:text-white bg-zinc-50 text-zinc-900 text-xs rounded shadow-lg">
+                  {label}
+                </span>
+              )}
+            </a>
+          </Link>
+        ))}
         {/* Separator */}
-        <div className="w-px h-6 bg-zinc-200" />
-
+        <div className="w-px h-6 dark:bg-zinc-500 bg-zinc-200" />
         {/* More Options Icon */}
         <button
           ref={ellipsisRef}
           onClick={handleEllipsisClick}
-          className="relative flex items-center justify-center w-8 h-8"
-          onMouseEnter={() => !isMobile() && setTooltip("More")}
-          onMouseLeave={() => setTooltip(null)}
+          className="relative flex items-center justify-center w-8 h-8 dark:hover:bg-zinc-800 hover:bg-zinc-200 rounded-md"
+          onMouseEnter={() =>
+            !isMobile() && setState((prev) => ({ ...prev, tooltip: "More" }))
+          }
+          onMouseLeave={() => setState((prev) => ({ ...prev, tooltip: null }))}
         >
-          <Ellipsis strokeWidth={1.5} className="w-6 h-6" />
-          {tooltip === "More" && (
-            <span className="absolute -top-10 left-1/2 transform -translate-x-1/2 px-2 py-1 bg-white text-black text-xs rounded shadow-lg">
+          <Ellipsis
+            strokeWidth={1.5}
+            className="w-6 h-6 dark:stroke-zinc-100 stroke-zinc-950"
+          />
+          {state.tooltip === "More" && (
+            <span className="absolute -top-10 left-1/2 transform -translate-x-1/2 px-2 py-1 dark:bg-zinc-900 dark:text-white bg-zinc-50 text-zinc-900 text-xs rounded shadow-lg">
               More
             </span>
           )}
@@ -148,10 +130,10 @@ const NavigationBar = () => {
       </div>
 
       {/* Sub Menu */}
-      {isSubMenuVisible && (
+      {state.isSubMenuVisible && (
         <div
           ref={subMenuRef}
-          className="absolute bottom-14 left-1/2 transform translate-x-2 bg-white rounded-lg shadow-2xl py-2"
+          className="absolute bottom-14 left-1/2 transform translate-x-2 border dark:border-zinc-800 border-zinc-300 dark:bg-zinc-900 dark:text-zinc-50 bg-zinc-50 text-zinc-950 rounded-lg shadow-2xl py-2"
         >
           <SubMenuLink href="https://twitter.com/DecryptTV" label="Twitter" />
           <SubMenuLink href="https://github.com/Decryptu" label="GitHub" />
@@ -167,7 +149,7 @@ const SubMenuLink = ({ href, label }: { href: string; label: string }) => (
     href={href}
     target="_blank"
     rel="noopener noreferrer"
-    className="flex items-center justify-center px-2 mx-2 py-2 hover:bg-zinc-200 hover:rounded-md"
+    className="flex items-center justify-center px-2 mx-2 py-2 dark:hover:bg-zinc-800 hover:bg-zinc-200 rounded-md"
   >
     {label}
     <ArrowUpRight strokeWidth={1.25} className="ml-1 w-4 h-4" />
