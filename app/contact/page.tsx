6584bac79e8e846af9ae2eea273e828706e3b87e
Decrypt <louis.vicomte@protonmail.com>
2024-04-24 19:59:19 +0200

    enhance contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -2,8 +2,17 @@
 import { Github, Mail, Twitter } from "lucide-react";
 import Link from "next/link";
 import { Card } from "../components/card";
+import { ReactElement } from "react";
 
-const socials = [
+// Define an interface for the social links
+interface SocialLinkProps {
+  icon: ReactElement; // Assuming icons are React elements
+  href: string;
+  handle: string;
+  label: string;
+}
+
+const socials: SocialLinkProps[] = [
   {
     icon: <Twitter size={20} />,
     href: "https://twitter.com/decrypttv",
@@ -24,38 +33,45 @@ const socials = [
   },
 ];
 
+const containerClasses =
+  "p-4 flex flex-col items-center gap-4 duration-700 group md:gap-8 md:py-24 lg:pb-48 md:p-16";
+const iconContainerClasses =
+  "relative z-10 flex items-center justify-center w-12 h-12 text-sm duration-1000 border rounded-full text-zinc-200 group-hover:text-white group-hover:bg-zinc-900 border-zinc-500 bg-zinc-900 group-hover:border-zinc-200 drop-shadow-orange";
+const handleClasses =
+  "text-xl font-medium duration-150 lg:text-3xl text-zinc-200 group-hover:text-white font-display";
+const labelClasses =
+  "mt-4 text-sm text-center duration-1000 text-zinc-400 group-hover:text-zinc-200";
+
+const SocialLink: React.FC<SocialLinkProps> = ({
+  icon,
+  href,
+  handle,
+  label,
+}) => (
+  <Card key={handle}>
+    <Link href={href} target="_blank" className="relative" legacyBehavior>
+      <div className={containerClasses}>
+        <span
+          className="absolute w-px h-2/3 bg-gradient-to-b from-zinc-500 via-zinc-500/50 to-transparent"
+          aria-hidden="true"
+        />
+        <span className={iconContainerClasses}>{icon}</span>
+        <div className="z-10 flex flex-col items-center">
+          <span className={handleClasses}>{handle}</span>
+          <span className={labelClasses}>{label}</span>
+        </div>
+      </div>
+    </Link>
+  </Card>
+);
+
 export default function Example() {
   return (
-    <div className=" bg-gradient-to-tl from-zinc-900/0 via-zinc-900 to-zinc-900/0">
+    <div className="bg-gradient-to-tl from-zinc-900/0 via-zinc-900 to-zinc-900/0">
       <div className="container flex items-center justify-center min-h-screen px-4 mx-auto">
         <div className="grid w-full grid-cols-1 gap-8 mx-auto mt-16 sm:mt-0 sm:grid-cols-3 lg:gap-16">
           {socials.map((s) => (
-            <Card key={s.handle}>
-              <Link
-                href={s.href}
-                target="_blank"
-                className="relative"
-                legacyBehavior
-              >
-                <div className="p-4 flex flex-col items-center gap-4 duration-700 group md:gap-8 md:py-24  lg:pb-48  md:p-16">
-                  <span
-                    className="absolute w-px h-2/3 bg-gradient-to-b from-zinc-500 via-zinc-500/50 to-transparent"
-                    aria-hidden="true"
-                  />
-                  <span className="relative z-10 flex items-center justify-center w-12 h-12 text-sm duration-1000 border rounded-full text-zinc-200 group-hover:text-white group-hover:bg-zinc-900 border-zinc-500 bg-zinc-900 group-hover:border-zinc-200 drop-shadow-orange">
-                    {s.icon}
-                  </span>
-                  <div className="z-10 flex flex-col items-center">
-                    <span className="text-xl font-medium duration-150 lg:text-3xl text-zinc-200 group-hover:text-white font-display">
-                      {s.handle}
-                    </span>
-                    <span className="mt-4 text-sm text-center duration-1000 text-zinc-400 group-hover:text-zinc-200">
-                      {s.label}
-                    </span>
-                  </div>
-                </div>
-              </Link>
-            </Card>
+            <SocialLink key={s.handle} {...s} />
           ))}
         </div>
       </div>
